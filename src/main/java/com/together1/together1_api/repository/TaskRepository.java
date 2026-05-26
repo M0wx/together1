6167908ebde8b6c416d36b485f84e3c4132aca82
Mox <manoa.andrianaliarimanana@gmail.com>
2026-05-26 22:01:28 +0300

    add some repository

---
+++ src/main/java/com/together1/together1_api/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.together1.together1_api.repository;
+
+import com.together1.together1_api.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task,Long> {
+}
